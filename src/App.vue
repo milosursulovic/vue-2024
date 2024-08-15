2ad59e3e435244f76b0e15638a711cb2442c1eaf
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-15 13:22:04 +0200

    Implement hero component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
-  import Navbar from './components/Navbar.vue';
+  import Navbar from '@/components/Navbar.vue';
+  import Hero from '@/components/Hero.vue';
 </script>
 
 <template>
   <Navbar />
+  <Hero />
   <h1 class="text-2xl">Vue Jobs</h1>
 </template>
