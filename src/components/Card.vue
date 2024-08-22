b83d2ba6addb3e886aa7e7c34e199141b1ea0d28
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-22 20:34:15 +0200

    Add job listing component and jobs data

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,16 @@
 <script setup>
-
-import { defineProps } from 'vue';
+import { defineProps } from "vue";
 
 defineProps({
-    bg: {
-        type: String,
-        default: 'bg-gray-100'
-    }
-})
-
+  bg: {
+    type: String,
+    default: "bg-gray-100",
+  },
+});
 </script>
 
 <template>
-    <div :class="`${bg} p-6 rounded-lg shadow-md`">
-        <slot></slot>
-    </div>
+  <div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+  </div>
 </template>
