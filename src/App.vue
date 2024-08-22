35f47336184762edcc036b90dddd54148bb72d7e
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-22 20:16:08 +0200

    Add card component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
   import Navbar from '@/components/Navbar.vue';
   import Hero from '@/components/Hero.vue';
+  import HomeCards from '@/components/HomeCards.vue';
 </script>
 
 <template>
   <Navbar />
   <Hero />
-  <h1 class="text-2xl">Vue Jobs</h1>
+  <HomeCards />
 </template>
