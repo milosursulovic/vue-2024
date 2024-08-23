9be6280cb149c40ce2f5c2fed997ffef6d3fc663
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-23 09:46:05 +0200

    Refactor job listings to reactive

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink } from "vue-router";
-import { ref, defineProps, onMounted } from "vue";
+import { reactive, ref, defineProps, onMounted } from "vue";
 import JobListing from "@/components/JobListing.vue";
 import axios from "axios";
 
@@ -12,14 +12,19 @@ defineProps({
   },
 });
 
-const jobs = ref([]);
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+});
 
 onMounted(async () => {
   try {
-    const response = await axios.get("http://localhost:5000/jobs");
-    jobs.value = response.data;
+    const response = await axios.get("http://localhost:8000/jobs");
+    state.jobs = response.data;
   } catch (error) {
     console.error("Error fetching jobs", error);
+  } finally {
+    state.isLoading = false;
   }
 });
 </script>
@@ -32,7 +37,7 @@ onMounted(async () => {
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
-          v-for="job in jobs.slice(0, limit || jobs.length)"
+          v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
           :job="job"
         />
