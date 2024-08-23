563d6396b950d3e577b45f92a0b62934e7760c0c
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-23 12:10:34 +0200

    Add toasts

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -2,6 +2,7 @@
 import router from "@/router";
 import { reactive } from "vue";
 import axios from "axios";
+import { useToast } from "vue-toastification";
 
 const form = reactive({
   type: "Full-Time",
@@ -16,6 +17,8 @@ const form = reactive({
     contactPhone: "",
   },
 });
+
+const toast = useToast();
 
 const handleSubmit = async () => {
   const newJob = {
@@ -35,9 +38,11 @@ const handleSubmit = async () => {
 
   try {
     const response = await axios.post("/api/jobs", newJob);
+    toast.success("Job Added Successfully");
     router.push(`/jobs/${response.data.id}`);
   } catch (error) {
     console.error("Error creating job", error);
+    toast.error("Job Was Not Added");
   }
 };
 </script>
